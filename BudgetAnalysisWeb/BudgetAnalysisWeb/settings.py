620c092fd48e37e791a222adf7caab6e4da68710
MikhailEmv <cheburusshka0303@gmail.com>
2023-03-15 01:45:06 +0500

    Little adjustment

--- BudgetAnalysisWeb/BudgetAnalysisWeb/settings.py
+++ BudgetAnalysisWeb/BudgetAnalysisWeb/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'BudgetAnalysisWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'users/templates/users'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- BudgetAnalysisWeb/BudgetAnalysisWeb/urls.py
+++ BudgetAnalysisWeb/BudgetAnalysisWeb/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='users/home.html'), name='home'),
     path('users/', include('users.urls'))
 ]
